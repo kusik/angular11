c9f9a7ae58fb1cad0b796dadf84956f1396787ca
CPCORP\hukku <hukku@coloplast.com>
2021-02-19 10:02:56 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { AddEditDepComponent } from './department/add-edit-dep/add-edit-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { SharedService } from './shared.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    AddEditDepComponent,
+    EmployeeComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [SharedService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
