82298e40d05f90f9e2dc6a27a10932033b21c5dd
CPCORP\hukku <hukku@coloplast.com>
2021-02-19 11:35:05 +0100

    add modal window

--- src/app/department/show-dep/show-dep.component.ts
+++ src/app/department/show-dep/show-dep.component.ts
@@ -10,11 +10,27 @@ export class ShowDepComponent implements OnInit {
   constructor(private service:SharedService) { }
 
   DepartmentList:any=[];
+  ModalTitle:string="";
+  ActivateAddEditDepComp:boolean=false;
+  dep:any;
 
   ngOnInit(): void {
     this.refreshDepList();
   }
 
+  addClick(){
+    this.dep={
+      DepartmentId:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department";
+    this.ActivateAddEditDepComp=true;
+  }
+
+  closeClick(){
+    this.ActivateAddEditDepComp=false;
+    this.refreshDepList();
+  }
 
   refreshDepList(){
     this.service.getDepList().subscribe(data=>{
